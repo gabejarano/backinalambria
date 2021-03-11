abb02d56cbd00c511fd5f3d5bccb23c839b0bf34
desarrollotelematica <innovacion.desarrollo@telematicaltda.com>
2021-03-11 14:11:09 -0500

    error conexion postresql

--- servicios/database/database.js
+++ servicios/database/database.js
@@ -1,21 +1,28 @@
 const Sequalize = require('sequelize');
 const config = require('config');
 //conecction of database using sequelize
-const sequelize = new Sequalize (
+const sequelize = new Sequalize(
     config.get('postresqlDB.DB'),
     config.get('postresqlDB.USER_DB'),
     config.get('postresqlDB.PASSWORD_DB'),
     {
         host: config.get('postresqlDB.HOST_DB'),
-        dialect:'postgres',
-        pool:{
-            max:20,
-            min:0,
-            require:30000,
-            idle:200000,
+        dialect: 'postgres',
+        ssl: true,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+            }
+        },
+        pool: {
+            max: 20,
+            min: 0,
+            require: 30000,
+            idle: 200000,
             acquire: 1000000
         },
-        loggin:false
+        loggin: false
     }
 )
-module.exports= sequelize;
+module.exports = sequelize;
